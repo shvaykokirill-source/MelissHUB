18f7597a90d4b8693ec580f7599d4909ff3e78d3
shvaykokirill-source <shvaykokirill@shostka-licey.com>
2026-09-21 19:38:26 +0300

    MelissHub.lua

--- MelissHub.lua
+++ MelissHub.lua
@@ -1,19 +1,22 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Terfiscript1/KitagawaStealAnEgg/refs/heads/main/KitagawaHubStealAnEgg"))()
 
-task.wait(0.1)
+task.wait(0.01)
 
-local hit = 0
-for _, v in pairs(game.CoreGui:GetDescendants()) do
-    if v:IsA("TextLabel") then
-        if v.Text == "KitagawaHub" then
-            v.Text = "MelissHub"
-        elseif v.Text == "Steal An Egg" then
-            hit = hit + 1
-            if hit == 1 then
-                v.Text = "by MelissXD"
-                v.TextSize = 10
-                v.TextColor3 = Color3.fromRGB(140, 140, 140)
+task.spawn(function()
+    while true do
+        for _, v in pairs(game.CoreGui:GetDescendants()) do
+            if v:IsA("TextLabel") then
+                if v.Text == "KitagawaHub" then
+                    v.Text = "MelissHub"
+                elseif v.Text == "KitagawaHub PatchLog" then
+                    v.Text = "MelissHub PatchLog"
+                elseif v.Text == "Steal An Egg" then
+                    v.Text = "by MelissXD"
+                    v.TextSize = 10
+                    v.TextColor3 = Color3.fromRGB(140, 140, 140)
+                end
             end
         end
+        task.wait(1)
     end
-end
+end)
